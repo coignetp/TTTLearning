4c86fc738779e729f3d6c707bf7ec34553bc4897
Paul Coignet <crushing.of.files@gmail.com>
2017-10-19 14:05:13 +0200

    Add (Player.py) Player class

---
+++ TTT_API/Player.py
@@ -0,0 +1,13 @@
+class Player:
+	"""
+	Basic class where all AI or Human player come from it.
+	"""
+
+	def __init__(self, idPlayer):
+		self.idPlayer = idPlayer
+
+	def play(board):
+		""" Makes the player play according to the actual board 
+			Returns the (x,y) position of the move """
+
+		return (-1,-1)
