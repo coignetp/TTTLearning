5b0891bbd88c380ad42b1e30746836cbfe65a3b7
Paul Coignet <crushing.of.files@gmail.com>
2017-11-04 23:34:44 +0100

    Set (Player.py) idPlayer parameter

--- TTT_API/Player.py
+++ TTT_API/Player.py
@@ -5,11 +5,13 @@ class Player:
 	Basic class where all AI or Human player come from it.
 	"""
 
-	def __init__(self, idPlayer: int):
-		self.idPlayer = idPlayer
+	def __init__(self):
+		self.idPlayer = 0
 
-	def play(self, board: Board) -> (int, int):
+	def play(self, board: Board, idPlayer: int) -> (int, int):
 		""" Makes the player play according to the actual board 
 			Returns the (x,y) position of the move """
+
+		self.idPlayer = idPlayer
 
 		return -1,-1
