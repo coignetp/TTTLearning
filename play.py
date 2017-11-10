65102f77f75d89c883391bc60f54bca08946b6fc
Paul Coignet <crushing.of.files@gmail.com>
2017-11-10 22:41:26 +0100

    Add (play.py) playing between 2 people

--- play.py
+++ play.py
@@ -5,6 +5,7 @@ from AI.AINeuralNetwork import AINeuralNetwork
 from sklearn.externals import joblib
 import argparse
 from pathlib import Path
+import numpy as np
 
 
 def getPlayer(pl):
@@ -31,6 +32,29 @@ def getPlayer(pl):
 
 	return Human()
 
+
+def getBoardSize(ai1, ai2):
+	"""
+	Return the board size thanks to the AI
+	Return -1 if there is a problem
+	"""
+
+	n1 = 0
+	n2 = 0
+
+	if isinstance(ai1, AINeuralNetwork):
+		n1 = len(ai1.clf.coefs_[0])
+	if isinstance(ai2, AINeuralNetwork):
+		n2 = len(ai2.clf.coefs_[0])
+
+	# Check if the IA are for different board size
+	if n1 != n2 and (n1 != 0 and n2 != 0):
+		print("Error : the AI are for different board size")
+
+	if n1 != 0:
+		return int(np.sqrt(n1))
+	return int(np.sqrt(max(n2, 3)))
+
 # Main function
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Make games of Tic Tac Toe between 2 people/AI.')
@@ -42,5 +66,27 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-	ai1 = getPlayer(args.player1)
-	ai2 = getPlayer(args.player2)
+	ai = []
+	ai.append(getPlayer(args.player1))
+	ai.append(getPlayer(args.player2))
+
+	size = getBoardSize(ai[0], ai[1])
+
+	# save the game results
+	results = [0] * 3
+
+	# play the games
+	args.n = max(args.n, 1)
+	for _ in range(args.n):
+		g = Game(size)
+		pl = 0
+
+		while g.board.getWinner() == -1:
+			g.playTurn(ai[pl].play, pl+1)
+
+			pl = (pl + 1)%2
+
+		results[g.board.getWinner()] += 1
+
+	if args.mute == None:
+		print(f'Results : {results}')
